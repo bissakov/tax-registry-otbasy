4baa726067b44d2db00a98b6964267968409d9d0
bissakov <78418192+bissakov@users.noreply.github.com>
2023-06-09 16:16:42 +0600

    Added quarterly report generation

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import os
 import warnings
 from os import makedirs
 from os.path import join
-from typing import List
+from typing import List, Optional, Tuple
 
 import dotenv
 import requests
@@ -14,6 +14,7 @@ from src.data_structures import Credentials, DateRange, Process, ReportInfo
 from src.logger import logger
 from src.utils import kill_all_processes
 
+
 logger = logger
 session = requests.Session()
 dotenv.load_dotenv()
@@ -23,8 +24,8 @@ process = Process(name='COLVIR', path=os.getenv('COLVIR_PROCESS_PATH'))
 
 
 def get_month_range(today: datetime.date) -> tuple:
-    year = today.year
-    month = today.month - 1
+    year = today.year if today.month != 1 else today.year - 1
+    month = today.month - 1 if today.month != 1 else 12
     first_day = datetime.datetime(year, month, 1)
 
     if month == 12:
@@ -38,19 +39,39 @@ def get_month_range(today: datetime.date) -> tuple:
     return first_day_str, last_day_str
 
 
-def get_report_info() -> List[ReportInfo]:
+def get_quarter_range(today: datetime.date) -> Optional[Tuple[str, str]]:
+    if today.day != 15 and today.month not in [1, 4, 7, 10]:
+        return
+    if today.month == 1:
+        first_day = datetime.date(today.year - 1, 10, 1)
+        last_day = datetime.date(today.year - 1, 12, 31)
+    else:
+        first_day = datetime.date(today.year, today.month - 3, 1)
+        last_day = datetime.date(today.year, today.month, 1) - datetime.timedelta(days=1)
+    return (
+        first_day.strftime('%d.%m.%Y'),
+        last_day.strftime('%d.%m.%Y')
+    )
+
+
+def get_report_info(today: datetime.date) -> List[ReportInfo]:
     report_types = ['Z_160_DEPOFNO200_02', 'Z_160_DEPOFNO200_05', 'Z_160_DEPOFNO200']
-    branches = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '11',
-                '12', '13', '14', '15', '17', '18', '19', '20', '21', '26']
+    branches = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '11', '12',
+                '13', '14', '15', '17', '18', '19', '20', '21', '26', '30', '31']
     russian_months = ['Январь', 'Февраль', 'Март', 'Апрель',
                       'Май', 'Июнь', 'Июль', 'Август',
                       'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
-    fserver_base_path = r'\\fserver\ДБУИО_Новая\ДБУИО_Общая папка\ДБУ_Информация УНУ\2023\ИПНи СН\200 по клиентам за 2023'
+    year = today.year if today.month != 1 else today.year - 1
+    fserver_base_path = fr'\\fserver\ДБУИО_Новая\ДБУИО_Общая папка\ДБУ_Информация УНУ\{year}\ИПНи СН\200 по клиентам за {year}'
     local_base_path = r'C:\Users\robot.ad\Desktop\tax registry\reports'
-    today = datetime.date.today()
     report_infos = []
 
-    parent_report_path = russian_months[today.month - 2]
+    if today.day == 5:
+        date_range = DateRange(*get_month_range(today))
+        parent_report_path = russian_months[today.month - 2]
+    else:
+        date_range = DateRange(*get_quarter_range(today))
+        parent_report_path = str((today.month - 1) / 3)
 
     for branch in branches:
         report_rel_folder_path = join(parent_report_path, branch)
@@ -65,11 +86,11 @@ def get_report_info() -> List[ReportInfo]:
                 report_local_full_path=join(local_base_path, report_rel_path),
                 report_fserver_folder_path=join(fserver_base_path, report_rel_folder_path),
                 report_fserver_full_path=join(fserver_base_path, report_rel_path),
-                range=DateRange(*get_month_range(today)),
+                range=date_range,
             ))
-            makedirs(report_infos[-1].report_local_folder_path, exist_ok=True)
-            makedirs(report_infos[-1].report_local_folder_path.replace('reports', 'converted_reports'),
-                     exist_ok=True)
+            # makedirs(report_infos[-1].report_local_folder_path, exist_ok=True)
+            # makedirs(report_infos[-1].report_local_folder_path.replace('reports', 'converted_reports'),
+            #          exist_ok=True)
             # makedirs(report_infos[-1].report_fserver_folder_path, exist_ok=True)
     return report_infos
 
@@ -79,7 +100,12 @@ def main() -> None:
 
     warnings.simplefilter(action='ignore', category=UserWarning)
 
-    report_infos = get_report_info()
+    # today = datetime.date.today()
+    today = datetime.date(2023, 1, 5)
+    if not (today.day == 5 or (today.day == 15 and today.month in [1, 4, 7, 10])):
+        return
+
+    report_infos = get_report_info(today)
 
     kill_all_processes(proc_name='COLVIR')
 
