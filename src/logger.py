8080f21c1f66c57c4c0bec0c65991800e1ab3626
Alikhan Bissakov <bissakov@yandex.ru>
2023-06-15 17:16:18 +0600

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -9,28 +9,28 @@ class LogFilter(logging.Filter):
     def filter(self, record):
         return 'Cannot retrieve text length for handle' not in record.getMessage()
 
-makedirs(r'D:\Work\python_rpa\tax-registry-otbasy\logs', exist_ok=True)
-actionlogger.enable()
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
 
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+def setup_logger() -> None:
+    makedirs(r'C:\Users\robot.ad\Desktop\tax registry\logs', exist_ok=True)
+    actionlogger.enable()
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
 
-file_handler = logging.FileHandler(
-    f'../logs/{datetime.date.today().strftime("%d.%m.%y")}.log',
-    encoding='utf-8'
-)
-file_handler.setLevel(logging.INFO)
-file_handler.setFormatter(formatter)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.INFO)
-stream_handler.setFormatter(formatter)
+    file_handler = logging.FileHandler(
+        f'../logs/{datetime.date.today().strftime("%d.%m.%y")}.log',
+        encoding='utf-8'
+    )
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
 
-logger.addFilter(LogFilter())
-logger.addHandler(file_handler)
-logger.addHandler(stream_handler)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(formatter)
 
-for name in logging.root.manager.loggerDict:
-    logger = logging.getLogger(name)
-    logger.addFilter(LogFilter())
+    pywinauto_logger = logging.getLogger('pywinauto')
+    pywinauto_logger.addFilter(LogFilter())
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
